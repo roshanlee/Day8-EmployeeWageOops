3e36c3d2935c75c181a2dea82f77da6a6e82d153
roshanlee <roshan26bhagat@gmail.com>
2021-07-14 18:45:59 +0530

    EmployeeWage Solution with Oops Concept

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.bridge.OOPsPractise;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+	private final static Logger logger = Logger.getLogger("UC1_Oops");
+
+	public static void main(String[] args) {
+		logger.log(Level.INFO, "Employee Wage Solution");
+	}
+}
